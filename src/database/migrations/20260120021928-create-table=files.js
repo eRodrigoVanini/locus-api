c6fa4230f64158ecbef97e5125b2f85d2d264dc4
eRodrigoVanini <arch.rvanini@gmail.com>
2026-01-19 23:25:43 -0300

    feat: creates files and users tables

--- src/database/migrations/20260120021928-create-table=files.js
+++ src/database/migrations/20260120021928-create-table=files.js
@@ -1,22 +1,39 @@
-'use strict';
+import Sequelize from 'sequelize';
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('files', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      original_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      file_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true, 
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface) {
+    await queryInterface.dropTable('files');
+  },
 };
