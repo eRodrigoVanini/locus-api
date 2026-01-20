d378546bb17cc06b9ea833c3f65e9a6fd00aa304
eRodrigoVanini <arch.rvanini@gmail.com>
2026-01-19 23:21:28 -0300

    feat: creates files migration

--- src/models/File.js
+++ src/models/File.js
@@ -1,44 +1,40 @@
-import Sequelize, { Model } from 'sequelize';
+import Sequelize from 'sequelize';
 
-class File extends Model {
-  static init(sequelize) {
-    super.init(
-      {
-        id: {
-          type: Sequelize.UUID,
-          defaultValue: Sequelize.UUIDV4,
-          primaryKey: true,
-        },
-        original_name: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        file_name: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        path: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        url: {
-          type: Sequelize.VIRTUAL,
-          get() {
-            // Pega a URL do servidor das variáveis de ambiente ou usa localhost como fallback
-            const appUrl = process.env.APP_URL
-            // Monta a URL final: http://localhost:PORT/files/nome-do-arquivo.jpg
-            return `${appUrl}/files/${this.path}`;
-          },
-        },
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('files', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
       },
-      {
-        sequelize,
-        tableName: 'files', // Garante que o Sequelize procure a tabela 'files'
-      }
-    );
+      original_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      file_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      // Geralmente armazenamos o caminho relativo ou o hash aqui
+      path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true, 
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
 
-    return this;
-  }
-}
-
-export default File;
+  async down(queryInterface) {
+    await queryInterface.dropTable('files');
+  },
+};
